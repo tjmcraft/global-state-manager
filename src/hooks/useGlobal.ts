5289ca34dd2fa9b662dac50cfdc0e47ce052d6e9
MakAndJo <68428951+MakAndJo@users.noreply.github.com>
2024-07-10 20:13:55 +0500

    Fix #2

--- src/hooks/useGlobal.ts
+++ src/hooks/useGlobal.ts
@@ -17,7 +17,7 @@ const updateContainer = <T, S>(selector: (state: T) => S, callback: Function, op
 				return;
 			}
 
-			if (nextState != void 0) {
+			if (nextState != undefined) {
 
 				const isArray = Array.isArray(prevState) || Array.isArray(nextState);
 				const shouldUpdate = isArray ?
